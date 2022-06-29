1dfb120ae58c2e87cf9e542f499877e0bb1616af
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-06-30 01:08:44 +0530

    Added the delete button inside todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -15,6 +15,14 @@ export default function Todo(props) {
         <button type="button" className="btn btn__danger">
           Delete <span className="visually-hidden">{props.name}</span>
         </button>
+        <button
+  type="button"
+  className="btn btn__danger"
+  onClick={() => props.deleteTask(props.id)}
+>
+  Delete <span className="visually-hidden">{props.name}</span>
+</button>
+
       </div>
     </li>
   
